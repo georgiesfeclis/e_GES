ec9ce7adb59e5e596577210362a89a4f97ac1e8f
Georgiana-Elena Sfeclis <Georgiana.Sfeclis@bf1systems.com>
2019-08-08 12:52:03 +0100

    added *.o and *.d to gitignore

--- Debug/Src/header.d
+++
@@ -1,9 +0,0 @@
-Src/header.o: ../Src/header.c \
- E:/embedded/e_GES/Inc/DataBuffers/uart_buffer.h \
- E:/embedded/e_GES/Inc/common.h E:/embedded/e_GES/Inc/header.h
-
-E:/embedded/e_GES/Inc/DataBuffers/uart_buffer.h:
-
-E:/embedded/e_GES/Inc/common.h:
-
-E:/embedded/e_GES/Inc/header.h:
